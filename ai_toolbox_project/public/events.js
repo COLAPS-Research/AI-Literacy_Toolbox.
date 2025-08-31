f0da4237effb30b3df0e30c3f2e1377612af2a3a
eberwild <kevinflotow@arcor.de>
2025-08-31 16:22:29 +0200

    Update events.js
    enabled e-mail section after a new tool ist added to the database

--- ai_toolbox_project/public/events.js
+++ ai_toolbox_project/public/events.js
@@ -133,7 +133,7 @@ submitButton.addEventListener("click" , async (event) => {
     }
 
   //------------------------------------------E-Mail section----------------------------------------------//
-  /*
+  
   // E-Mail Adresse aus dem InputFeld holen
   const email = getEmail();
 
@@ -164,6 +164,6 @@ submitButton.addEventListener("click" , async (event) => {
       console.error('Fehler beim Senden:', error);
       alert('Beim Senden ist ein Fehler aufgetreten.');
     }
-      */
+      
   });
   
